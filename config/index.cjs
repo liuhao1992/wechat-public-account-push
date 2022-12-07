6b0ec578933d577c46d95b06734c3b6ee58693e4
liuhao1992 <82426158+liuhao1992@users.noreply.github.com>
2022-12-07 16:14:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -38,6 +38,9 @@ const USER_CONFIG = {
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
         },
+        {
+          type: '节日', name: '相识纪念日2', year: '2020', date: '09-03',
+        },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
